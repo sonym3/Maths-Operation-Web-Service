14acf62e5997fdcc881e8e474894c8ea61f940bf
sonym3 <Bony#9400>
2019-10-05 11:11:40 -0400

    Initial commit

---
+++ src/java/maths/MathsApplication.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maths;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author 1895268
+ */
+@Path("mobile")
+public class MathsApplication {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of MathsApplication
+     */
+    public MathsApplication() {
+    }
+
+    /**
+     * Retrieves representation of an instance of maths.MathsApplication
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("pro&{value1}&{value2}&{value3}&{value4}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getText(@PathParam("value1") String v1,
+            @PathParam("value2") int v2,
+            @PathParam("value3") int v3,
+            @PathParam("value4") int v4) {
+        String result = String.valueOf(v2);
+        if(v1.equalsIgnoreCase("ap")){
+            for(int i=0;i<v4;i++){
+                 result += " " + (v2+=v3);
+                 
+            }
+            
+        }
+        else if(v1.equalsIgnoreCase("gp")){
+            int curr_term; 
+            for (int i = 0; i <v4; i++) { 
+                curr_term = v2 * (int)Math.pow(v3, i); 
+                result += " " + curr_term; 
+            }
+        }
+        else
+            return "";
+        
+        
+         
+        //TODO return proper representation object
+        
+       
+     return "The resut is :" + result;
+    }
+
+    
+}
